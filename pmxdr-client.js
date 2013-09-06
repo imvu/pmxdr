79f9afff93f9d92289bfe74e22258cac330c186f
Sufian Rhazi <srhazi@imvu.com>
2013-09-06 10:29:03 -0700

    pmxdr-client.js is an imvujs module

--- pmxdr-client.js
+++ pmxdr-client.js
@@ -8,26 +8,23 @@
 
 /*! @source http://purl.eligrey.com/github/pmxdr/blob/master/pmxdr-client.js*/
 
-(function() {
-    
-    var window = this;
-
+module({}, function () {
     if (typeof window.opera !== "undefined" && parseInt(window.opera.version(), 10) === 9) // Opera 9.x MessageEvent.origin fix (only for http:, not https:)
         Event.prototype.__defineGetter__("origin", function(){
             return "http://" + this.domain;
         });
-    
+
     function pmxdr(host, onload) {
         var instance = this; // for YUI compressor
         instance.iFrame = document.createElement("iframe"); // interface frame
         instance.iFrame.style.display = "none";
         instance.origin = host.replace(pmxdr.originRegex, "$1");
-        
+
         function onloadHandler() {
             if (typeof instance.onload === "function")
                 instance.onload();
         }
-        
+
         if (instance.iFrame.addEventListener)
             instance.iFrame.addEventListener("load", onloadHandler, false);
         else if (instance.iFrame.attachEvent)
@@ -39,7 +36,7 @@
             instance.init();
         }
     }
-    
+
     pmxdr.originRegex = /^([\w\-]+:\/*\[?[\w\.:\-]+\]?(?::\d+)?).*/; // RegExp.$1 = protocol+host+port (the square brackets are for ipv6)
     pmxdr.request = function(req) {
         if (typeof req === "string")
@@ -50,16 +47,16 @@
                 requests.push(pmxdr.request(req[i]));
             return requests;
         }
-        
+
         var pmxdrInstance = new pmxdr(req.uri),
         callback = req.callback;
         req.id = pmxdr.getSafeID();
-        
+
         req.callback = function(response) {
             if (typeof callback === "function") callback.call(this, response);
             this.unload();
         };
-        
+
         pmxdrInstance.onload = function() {
             this.request(req);
         };
@@ -73,32 +70,32 @@
     };
     // parent for interface frames
     pmxdr.interfaceFrameParent = document.documentElement||document.getElementsByTagName("head")[0]||document.body||document.getElementsByTagName("body")[0];
-        
+
     pmxdr.getSafeID = function() { // generate a random key that doesn't collide with any existing keys
         var randID = Math.random().toString().substr(2); // Generate a random number, make it a string, and cut off the "0." to make it look nice
         if (typeof pmxdr.requests[randID] === "undefined") return randID; // key doesn't collide
         else return pmxdr.getSafeID();
     };
     pmxdr.prototype = {
-    
+
         init: function(onload) { // load or reload iframe
             if (typeof onload === "function")
                 this.onload = onload;
             if (this.iFrame.parentNode) this.unload(); // in case init is being called to re-init
             pmxdr.interfaceFrameParent.appendChild(this.iFrame);
         },
-        
+
         unload: function() { // remove iframe
             pmxdr.interfaceFrameParent.removeChild(this.iFrame);
         },
-        
+
         defaultRequestMethod: "GET", // default request method
         //defaultContentType: "application/x-www-form-urlencoded", default content-type header (ie. POST requests, ect.)
         //defaultTimeout: 60000, // optional default timeout at which the request recives a TIMEOUT error (60000 ms = 1 minute)
-        
+
         request: function(req) { // send a request to a pmxdr host
             var requests = [], instance = this;
-            
+
             if (Object.prototype.toString.call(req) === "[object Array]") { // handle array of requests
                 for (var i=0; i<req.length; i++)
                     requests.push(instance.request(req[i]));
@@ -109,9 +106,9 @@
             method      = req.method      || instance.defaultRequestMethod,
             timeout     = req.timeout     || instance.defaultTimeout,
             contentType = req.contentType || instance.defaultContentType;
-            
+
             method = method.toUpperCase();
-            
+
             pmxdr.requests[id] = {
                 origin: instance.origin,
                 remove: function() {
@@ -122,7 +119,7 @@
                     pmxdr.requests[id].remove();
                 }
             };
-    
+
             function timeoutCallback() { // give callback a TIMEOUT error
                 if (pmxdr.requests[id] && pmxdr.requests[id].callback)
                     pmxdr.requests[id].callback({
@@ -131,13 +128,13 @@
                         error   : "TIMEOUT"
                     });
             }
-            
+
             if (contentType) {
                 if (!req.headers)
                     req.headers = {};
                 req.headers["Content-Type"] = contentType.toString();
             }
-            
+
             instance.iFrame.contentWindow.postMessage(JSON.stringify({
                 pmxdr   : true,
                 method  : method,
@@ -147,7 +144,7 @@
                 id      : id
             }), instance.origin);
             if (timeout) setTimeout(timeoutCallback, timeout);
-    
+
             return {
                 abort: pmxdr.requests[id].remove
             };
@@ -194,9 +191,7 @@
             window.removeEventListener("message", pmxdrResponseHandler, false);
         else if (window.detachEvent)
             window.detachEvent("onmessage", pmxdrResponseHandler);
-        
-        delete window.pmxdr;
     };
 
-    window.pmxdr = pmxdr;
-})();
+    return pmxdr;
+});
